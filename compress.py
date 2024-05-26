4ae127f8e3d194d9f397592715753ea7dfac6e97
EntropyF <fujiajun73@gmail.com>
2024-05-26 20:08:56 +0100

    start selection of compression method

---
+++ compress.py
@@ -0,0 +1,17 @@
+import warnings
+import inspect
+import matplotlib.pyplot as plt
+import IPython.display
+import numpy as np
+from cued_sf2_lab.familiarisation import load_mat_img, plot_image
+from cued_sf2_lab.laplacian_pyramid import quantise
+
+# Initialise three images
+lighthouse, _ = load_mat_img(img='lighthouse.mat', img_info='X')
+bridge, _ = load_mat_img(img='bridge.mat', img_info='X')
+flamingo, _ = load_mat_img(img='flamingo.mat', img_info='X')
+
+fig, axs = plt.subplots(1, 3)
+plot_image(lighthouse, ax=axs[0])
+plot_image(bridge, ax=axs[1])
+plot_image(flamingo, ax=axs[2])
