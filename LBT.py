142bef94f4f2734a2b8a3481a34f4e6f70da3653
EntropyF <fujiajun73@gmail.com>
2024-06-05 19:53:14 +0100

    SSIM consist perfectly with rms error

--- LBT.py
+++ LBT.py
@@ -40,6 +40,32 @@ def ILBT(Y: np.ndarray, N: int, s: float) -> np.ndarray:
     Zp[t,:] = colxfm(Zp[t,:], Pr.T)
     return Zp
 
+# Calculate rms error
+def lbt_rms_error_gap(step, X, N, n=1.0):
+    Y = LBT(X, N, s)
+    rise1 = step * n
+    Yq = quantise(Y, step, rise1)
+    Z = ILBT(Yq, N, s)
+    lbt_rms_error = np.std(X - Z)
+    rmsX = np.std(X-quantise(X, 17))
+    return abs(lbt_rms_error- rmsX)
+
+# Find opt step size
+def optimize_dct_step_size(X, N, n=1.0):
+    result = minimize_scalar(
+        lbt_rms_error_gap, 
+        args=(X, N, n),
+        bounds=(0.1, 50), 
+        method='bounded'
+        )
+    return result.x
+
+def reconstruct_lbt(step, X, N, s):
+    Y = LBT(X, N, s)
+    Yq = quantise(Y, step, step)
+    Z = ILBT(Yq, N, s)
+    return Z
+
 # Yl = LBT(Xl, 8, s=np.sqrt(2))
 # Zl = ILBT(Yl, 8, s=np.sqrt(2))
 
@@ -49,3 +75,16 @@ def ILBT(Y: np.ndarray, N: int, s: float) -> np.ndarray:
 # fig, ax = plt.subplots()
 # plot_image(Zl, ax=ax)
 # plt.show()
+
+s = np.sqrt(2)
+X_test = Xb
+N = 4
+step_opt = optimize_dct_step_size(X_test, N)
+print(f'optimized step size: {step_opt:.4f}')
+Z = reconstruct_lbt(step_opt, X_test, N, s)
+ssim_score = calculate_ssim(X_test, Z)
+print(f"SSIM between the images: {ssim_score:.4f}")
+
+fig, ax = plt.subplots()
+plot_image(Z, ax=ax)
+plt.show()
